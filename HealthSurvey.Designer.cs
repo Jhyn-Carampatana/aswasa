ab46e9cb0cd8201d0c8a981d5184a5488cdcd770
Jhyn-Carampatana <152131858+Jhyn-Carampatana@users.noreply.github.com>
2025-03-29 00:08:56 +0800

    Create HealthSurvey.Designer.cs

---
+++ HealthSurvey.Designer.cs
@@ -0,0 +1,343 @@
+  namespace HealthSurvey
+{
+    partial class HealthSurvey
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.DateTimePicker dtpBirthdate;
+        private System.Windows.Forms.NumericUpDown numAge;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.CheckBox chkBP;
+        private System.Windows.Forms.CheckBox chkDiabetes1;
+        private System.Windows.Forms.CheckBox chkDiabetes2;
+        private System.Windows.Forms.CheckBox chkGout;
+        private System.Windows.Forms.NumericUpDown numHeight;
+        private System.Windows.Forms.NumericUpDown numWeight;
+        private System.Windows.Forms.NumericUpDown numCurrentBMI;
+        private System.Windows.Forms.NumericUpDown numTargetBMI;
+        private System.Windows.Forms.TextBox txtBedtime;
+        private System.Windows.Forms.TextBox txtWakeTime;
+        private System.Windows.Forms.NumericUpDown numSleepHours;
+        private System.Windows.Forms.RadioButton rbRestedYes;
+        private System.Windows.Forms.RadioButton rbRestedNo;
+        private System.Windows.Forms.TextBox txtWork;
+        private System.Windows.Forms.RadioButton rbEnjoyWorkYes;
+        private System.Windows.Forms.RadioButton rbEnjoyWorkNo;
+        private System.Windows.Forms.NumericUpDown numStress;
+        private System.Windows.Forms.TextBox txtFirstMeal;
+        private System.Windows.Forms.TextBox txtLastMeal;
+        private System.Windows.Forms.NumericUpDown numMealsPerDay;
+        private System.Windows.Forms.ComboBox cmbExercise;
+        private System.Windows.Forms.ComboBox cmbSmoking;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.RichTextBox rtbSummary;
+
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.dtpBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.numAge = new System.Windows.Forms.NumericUpDown();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.chkBP = new System.Windows.Forms.CheckBox();
+            this.chkDiabetes1 = new System.Windows.Forms.CheckBox();
+            this.chkDiabetes2 = new System.Windows.Forms.CheckBox();
+            this.chkGout = new System.Windows.Forms.CheckBox();
+            this.numHeight = new System.Windows.Forms.NumericUpDown();
+            this.numWeight = new System.Windows.Forms.NumericUpDown();
+            this.numCurrentBMI = new System.Windows.Forms.NumericUpDown();
+            this.numTargetBMI = new System.Windows.Forms.NumericUpDown();
+            this.txtBedtime = new System.Windows.Forms.TextBox();
+            this.txtWakeTime = new System.Windows.Forms.TextBox();
+            this.numSleepHours = new System.Windows.Forms.NumericUpDown();
+            this.rbRestedYes = new System.Windows.Forms.RadioButton();
+            this.rbRestedNo = new System.Windows.Forms.RadioButton();
+            this.txtWork = new System.Windows.Forms.TextBox();
+            this.rbEnjoyWorkYes = new System.Windows.Forms.RadioButton();
+            this.rbEnjoyWorkNo = new System.Windows.Forms.RadioButton();
+            this.numStress = new System.Windows.Forms.NumericUpDown();
+            this.txtFirstMeal = new System.Windows.Forms.TextBox();
+            this.txtLastMeal = new System.Windows.Forms.TextBox();
+            this.numMealsPerDay = new System.Windows.Forms.NumericUpDown();
+            this.cmbExercise = new System.Windows.Forms.ComboBox();
+            this.cmbSmoking = new System.Windows.Forms.ComboBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.rtbSummary = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCurrentBMI)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTargetBMI)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSleepHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStress)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMealsPerDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(0, 0);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 23);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(0, 0);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 23);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(0, 0);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(0, 0);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 0;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(0, 0);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 0;
+            // 
+            // dtpBirthdate
+            // 
+            this.dtpBirthdate.Location = new System.Drawing.Point(0, 0);
+            this.dtpBirthdate.Name = "dtpBirthdate";
+            this.dtpBirthdate.Size = new System.Drawing.Size(200, 20);
+            this.dtpBirthdate.TabIndex = 0;
+            // 
+            // numAge
+            // 
+            this.numAge.Location = new System.Drawing.Point(0, 0);
+            this.numAge.Name = "numAge";
+            this.numAge.Size = new System.Drawing.Size(120, 20);
+            this.numAge.TabIndex = 0;
+            // 
+            // rbMale
+            // 
+            this.rbMale.Location = new System.Drawing.Point(0, 0);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(104, 24);
+            this.rbMale.TabIndex = 0;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.Location = new System.Drawing.Point(0, 0);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(104, 24);
+            this.rbFemale.TabIndex = 0;
+            // 
+            // chkBP
+            // 
+            this.chkBP.Location = new System.Drawing.Point(0, 0);
+            this.chkBP.Name = "chkBP";
+            this.chkBP.Size = new System.Drawing.Size(104, 24);
+            this.chkBP.TabIndex = 0;
+            // 
+            // chkDiabetes1
+            // 
+            this.chkDiabetes1.Location = new System.Drawing.Point(0, 0);
+            this.chkDiabetes1.Name = "chkDiabetes1";
+            this.chkDiabetes1.Size = new System.Drawing.Size(104, 24);
+            this.chkDiabetes1.TabIndex = 0;
+            // 
+            // chkDiabetes2
+            // 
+            this.chkDiabetes2.Location = new System.Drawing.Point(0, 0);
+            this.chkDiabetes2.Name = "chkDiabetes2";
+            this.chkDiabetes2.Size = new System.Drawing.Size(104, 24);
+            this.chkDiabetes2.TabIndex = 0;
+            // 
+            // chkGout
+            // 
+            this.chkGout.Location = new System.Drawing.Point(0, 0);
+            this.chkGout.Name = "chkGout";
+            this.chkGout.Size = new System.Drawing.Size(104, 24);
+            this.chkGout.TabIndex = 0;
+            // 
+            // numHeight
+            // 
+            this.numHeight.Location = new System.Drawing.Point(0, 0);
+            this.numHeight.Name = "numHeight";
+            this.numHeight.Size = new System.Drawing.Size(120, 20);
+            this.numHeight.TabIndex = 0;
+            // 
+            // numWeight
+            // 
+            this.numWeight.Location = new System.Drawing.Point(0, 0);
+            this.numWeight.Name = "numWeight";
+            this.numWeight.Size = new System.Drawing.Size(120, 20);
+            this.numWeight.TabIndex = 0;
+            // 
+            // numCurrentBMI
+            // 
+            this.numCurrentBMI.Location = new System.Drawing.Point(0, 0);
+            this.numCurrentBMI.Name = "numCurrentBMI";
+            this.numCurrentBMI.Size = new System.Drawing.Size(120, 20);
+            this.numCurrentBMI.TabIndex = 0;
+            // 
+            // numTargetBMI
+            // 
+            this.numTargetBMI.Location = new System.Drawing.Point(0, 0);
+            this.numTargetBMI.Name = "numTargetBMI";
+            this.numTargetBMI.Size = new System.Drawing.Size(120, 20);
+            this.numTargetBMI.TabIndex = 0;
+            // 
+            // txtBedtime
+            // 
+            this.txtBedtime.Location = new System.Drawing.Point(0, 0);
+            this.txtBedtime.Name = "txtBedtime";
+            this.txtBedtime.Size = new System.Drawing.Size(100, 20);
+            this.txtBedtime.TabIndex = 0;
+            // 
+            // txtWakeTime
+            // 
+            this.txtWakeTime.Location = new System.Drawing.Point(0, 0);
+            this.txtWakeTime.Name = "txtWakeTime";
+            this.txtWakeTime.Size = new System.Drawing.Size(100, 20);
+            this.txtWakeTime.TabIndex = 0;
+            // 
+            // numSleepHours
+            // 
+            this.numSleepHours.Location = new System.Drawing.Point(0, 0);
+            this.numSleepHours.Name = "numSleepHours";
+            this.numSleepHours.Size = new System.Drawing.Size(120, 20);
+            this.numSleepHours.TabIndex = 0;
+            // 
+            // rbRestedYes
+            // 
+            this.rbRestedYes.Location = new System.Drawing.Point(0, 0);
+            this.rbRestedYes.Name = "rbRestedYes";
+            this.rbRestedYes.Size = new System.Drawing.Size(104, 24);
+            this.rbRestedYes.TabIndex = 0;
+            // 
+            // rbRestedNo
+            // 
+            this.rbRestedNo.Location = new System.Drawing.Point(0, 0);
+            this.rbRestedNo.Name = "rbRestedNo";
+            this.rbRestedNo.Size = new System.Drawing.Size(104, 24);
+            this.rbRestedNo.TabIndex = 0;
+            // 
+            // txtWork
+            // 
+            this.txtWork.Location = new System.Drawing.Point(0, 0);
+            this.txtWork.Name = "txtWork";
+            this.txtWork.Size = new System.Drawing.Size(100, 20);
+            this.txtWork.TabIndex = 0;
+            // 
+            // rbEnjoyWorkYes
+            // 
+            this.rbEnjoyWorkYes.Location = new System.Drawing.Point(0, 0);
+            this.rbEnjoyWorkYes.Name = "rbEnjoyWorkYes";
+            this.rbEnjoyWorkYes.Size = new System.Drawing.Size(104, 24);
+            this.rbEnjoyWorkYes.TabIndex = 0;
+            // 
+            // rbEnjoyWorkNo
+            // 
+            this.rbEnjoyWorkNo.Location = new System.Drawing.Point(0, 0);
+            this.rbEnjoyWorkNo.Name = "rbEnjoyWorkNo";
+            this.rbEnjoyWorkNo.Size = new System.Drawing.Size(104, 24);
+            this.rbEnjoyWorkNo.TabIndex = 0;
+            // 
+            // numStress
+            // 
+            this.numStress.Location = new System.Drawing.Point(0, 0);
+            this.numStress.Name = "numStress";
+            this.numStress.Size = new System.Drawing.Size(120, 20);
+            this.numStress.TabIndex = 0;
+            // 
+            // txtFirstMeal
+            // 
+            this.txtFirstMeal.Location = new System.Drawing.Point(0, 0);
+            this.txtFirstMeal.Name = "txtFirstMeal";
+            this.txtFirstMeal.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstMeal.TabIndex = 0;
+            // 
+            // txtLastMeal
+            // 
+            this.txtLastMeal.Location = new System.Drawing.Point(0, 0);
+            this.txtLastMeal.Name = "txtLastMeal";
+            this.txtLastMeal.Size = new System.Drawing.Size(100, 20);
+            this.txtLastMeal.TabIndex = 0;
+            // 
+            // numMealsPerDay
+            // 
+            this.numMealsPerDay.Location = new System.Drawing.Point(0, 0);
+            this.numMealsPerDay.Name = "numMealsPerDay";
+            this.numMealsPerDay.Size = new System.Drawing.Size(120, 20);
+            this.numMealsPerDay.TabIndex = 0;
+            // 
+            // cmbExercise
+            // 
+            this.cmbExercise.Location = new System.Drawing.Point(0, 0);
+            this.cmbExercise.Name = "cmbExercise";
+            this.cmbExercise.Size = new System.Drawing.Size(121, 21);
+            this.cmbExercise.TabIndex = 0;
+            // 
+            // cmbSmoking
+            // 
+            this.cmbSmoking.Location = new System.Drawing.Point(0, 0);
+            this.cmbSmoking.Name = "cmbSmoking";
+            this.cmbSmoking.Size = new System.Drawing.Size(121, 21);
+            this.cmbSmoking.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(0, 0);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // rtbSummary
+            // 
+            this.rtbSummary.Location = new System.Drawing.Point(0, 0);
+            this.rtbSummary.Name = "rtbSummary";
+            this.rtbSummary.Size = new System.Drawing.Size(100, 96);
+            this.rtbSummary.TabIndex = 3;
+            this.rtbSummary.Text = "";
+            // 
+            // HealthSurvey
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(888, 424);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.rtbSummary);
+            this.Font = new System.Drawing.Font("Arial", 10F);
+            this.Name = "HealthSurvey";
+            this.Text = "Health Survey";
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCurrentBMI)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTargetBMI)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSleepHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStress)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMealsPerDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
